d04b213a0d18684053c4ca794bc61ed654a62bfb
Elena-IT <elena.afanasieva89@gmail.com>
2020-11-15 12:50:26 +0200

    Решена задача №5

---
+++ JS/task-5.js
@@ -0,0 +1,10 @@
+//Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input),
+//подставляет его текущее значение в span#name - output.Если инпут пустой, 
+//в спане должна отображаться строка 'незнакомец'.
+
+const inputRef = document.querySelector('#name-input');
+const inputNameRef = document.querySelector('span#name-output')
+
+inputRef.addEventListener('input', event =>{
+    inputNameRef.textContent = event.target.value;
+})
