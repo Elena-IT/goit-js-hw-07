36874201aa0f157317d6255264f61274037ea1a4
Elena-IT <elena.afanasieva89@gmail.com>
2020-11-15 12:15:04 +0200

    Решена задача 2

--- JS/task-2.js
+++ JS/task-2.js
@@ -9,5 +9,23 @@ const ingredients = [
 
 //Напиши скрипт, 
 //который для каждого элемента массива ingredients создаст отдельный li,
+const createIngredientsListItem = item => {
+  const listItem = document.createElement('li');
+
+  listItem.textContent = item;
+
+  return listItem;
+};
+
 //после чего вставит все li за одну операцию в список ul.ingredients.
 //Для создания DOM - узлов используй document.createElement().
+
+const listItemsArr = ingredients.map(ingredient =>
+  createIngredientsListItem(ingredient),
+);
+
+const list = document.querySelector('#ingredients');
+
+list.append(...listItemsArr);
+
+console.log('Динамически созданный список ингредиентов: ', list);
